0b29e0f77515207ae5f2bbeaf116366298ce64df
esplo <esplo@users.noreply.github.com>
2015-09-14 23:56:58 +0900

    [change] if phantomJSPath is empty, "phantomjs.binary.path" remains empty

--- src/main/scala/esplo/DriverManager.scala
+++ src/main/scala/esplo/DriverManager.scala
@@ -9,7 +9,8 @@ class DriverManager(val phantomJSPath: String) {
 
   val caps = new DesiredCapabilities()
   // 存在しないパスの場合は、ドライバ生成時に例外を投げる
-  caps.setCapability("phantomjs.binary.path", phantomJSPath)
+  if(!phantomJSPath.isEmpty)  // 空の場合は設定しない
+    caps.setCapability("phantomjs.binary.path", phantomJSPath)
 
   val driver = new PhantomJSDriver(caps)
 
